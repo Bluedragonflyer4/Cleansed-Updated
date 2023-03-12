b05e21fade559d85e6a2bb492bb21d709d0331e5
Bluedragonflyer <tootsyum@gmail.com>
2023-03-12 12:08:04 +0000

    Started work on fixing #3

--- World/Scripts/Town.gd
+++ World/Scripts/Town.gd
@@ -5,30 +5,30 @@ const projectile = preload("res://Player/Scenes/Projectile.tscn")
 var projectiles = 0
 
 func _ready() -> void:
-	$PauseScreen.visible = false
-	$PauseScreen/ResumeButton.visible = false
-	$PauseScreen/Shade.visible = false
-	$PauseScreen/ExitButton.visible = false
-	$PauseScreen/OptionButton.visible = false
+	$PauseScreen.set_visible(false)
+	$PauseScreen/ResumeButton.set_visible(false)
+	$PauseScreen/Shade.setVisible = false
+	$PauseScreen/ExitButton.setVisible = false
+	$PauseScreen/OptionButton.setVisible = false
 
 func _physics_process(_delta: float) -> void:
 	if Input.get_action_strength("Esc") and $PauseTimer.time_left == 0:
 		get_tree().paused = true
-		$PauseScreen.visible = true
-		$PauseScreen/ResumeButton.visible = true
-		$PauseScreen/Shade.visible = true
-		$PauseScreen/ExitButton.visible = true
-		$PauseScreen/OptionButton.visible = true
+		$PauseScreen.setVisible = true
+		$PauseScreen/ResumeButton.setVisible = true
+		$PauseScreen/Shade.setVisible = true
+		$PauseScreen/ExitButton.setVisible = true
+		$PauseScreen/OptionButton.setVisible = true
 		
 		
 	
 		
 func _on_Button_pressed() -> void:
 	get_tree().paused = false
-	$PauseScreen/ResumeButton.visible = false
-	$PauseScreen/Shade.visible = false
-	$PauseScreen/ExitButton.visible = false
-	$PauseScreen/OptionButton.visible = false
+	$PauseScreen/ResumeButton.setVisible = false
+	$PauseScreen/Shade.setVisible = false
+	$PauseScreen/ExitButton.setVisible = false
+	$PauseScreen/OptionButton.setVisible = false
 
 
 func _on_ExitButton_pressed() -> void:
